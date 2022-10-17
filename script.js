f132b973e8ab81de501e14e05cf0a8ebd4276011
AStandish <112442942+AStandish@users.noreply.github.com>
2022-10-16 20:56:41 -0400

    added html starter code, added style, added js starter

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const startbutton = document.getElementById("startbutton")
+const startdiv = document.getElementById("startdiv")
+const formdiv = document.getElementById("formdiv")
+const scorediv = document.getElementById("scorediv")
+const questiondiv= document.getElementById("questiondiv")
+const questionheader= document.getElementById("questionheader")
+const choice1= document.getElementById("choice1")
+const choice2= document.getElementById("choice2")
+const choice3= document.getElementById("choice3")
+const choice4= document.getElementById("choice4")
 
+startbutton.addEventListener("click", function(){
+    startdiv.classList.add("hidden")
+    questiondiv.classList.remove("hidden")
+    questionheader.textContent="ssss"
+}) 
